6ae88bfbb37a19cd37419d62c17f04aa832aef8e
sohir <sohirhashem@gmail.com>
2022-08-28 12:14:27 +0200

    Build the DTO

--- hero/hero-datasource/build.gradle.kts
+++ hero/hero-datasource/build.gradle.kts
@@ -1,6 +1,18 @@
-apply{
+import org.jetbrains.kotlin.ir.backend.js.ic.ModuleName
+
+apply {
     from("$rootDir/library-build.gradle")
 }
-dependencies{
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
+dependencies {
+    //expand the model of hero data source (API/Cache response model)
+    "implementation"(project(Modules.heroDomain))
+
+    //Ktor library implementation
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.clientSerialization)
+    "implementation"(Ktor.android)
 
 }
--- hero/hero-datasource/src/main/java/com/idbs/hero_datasource/PlaceHolder.kt
+++
@@ -1,4 +0,0 @@
-package com.idbs.hero_datasource
-
-class PlaceHolder {
-}
